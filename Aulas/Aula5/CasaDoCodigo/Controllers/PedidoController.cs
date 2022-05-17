35e50b61b77499a1d3915dad77548b0b4d41c4b1
LAPTOP-1EG0V295\cctie <cctiely97@gmail.com>
2022-05-17 20:14:47 -0300

    Versão 1

---
+++ Aulas/Aula5/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,59 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IProdutoRepository produtoRepository;
+        private readonly IPedidoRepository pedidoRepository;
+
+        public PedidoController(IProdutoRepository produtoRepository,
+            IPedidoRepository pedidoRepository)
+        {
+            this.produtoRepository = produtoRepository;
+            this.pedidoRepository = pedidoRepository;
+        }
+
+        public IActionResult Carrossel()
+        {
+            return View(produtoRepository.GetProdutos());
+        }
+
+        public IActionResult Carrinho(string codigo)
+        {
+            if (!string.IsNullOrEmpty(codigo))
+            {
+                pedidoRepository.AddItem(codigo);
+            }
+
+            Pedido pedido = pedidoRepository.GetPedido();
+            return View(pedido.Itens);
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            Pedido pedido = pedidoRepository.GetPedido();
+            return View(pedido);
+        }
+        [HttpPost] //para definir que a requisição é post
+        public void Updatequantidade([FromBody] ItemPedido itemPedido) //os métodos do projeto
+        {
+          //FromBody indica que os dadoss irão vim no corpo da requisição, co caso o item pedidos também vem o data dentro
+          // com a quantidade e id do pedido 
+
+            
+        }
+
+    }
+}
